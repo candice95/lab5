d81205d0d3331b3c608c1c666948e114a5f1918f
vagrant <vagrant@precise32.(none)>
2015-02-05 21:11:57 +0000

    lab5

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,11 +10,14 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$('.friends a').click(function() {
+		event.preventDefault();
+		$(this).text(anagrammedName($(this).text()));
+	});
 }
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
 	} 
